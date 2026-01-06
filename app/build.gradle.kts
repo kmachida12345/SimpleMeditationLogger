619d0dd8c340145e907a991255758cdfc624d26e
machida kohei <kmachida12345@gmail.com>
2026-01-07 00:37:24 +0900

    feat: Update version code to 3 and version name to 1.2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.github.kmachida12345.simplemeditationlogger"
         minSdk = 34
         targetSdk = 36
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         
